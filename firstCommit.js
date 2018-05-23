d0f9b37e96ad5620f5b743ff916cf82a7523a05e
Phuc Dao <kanidd20@gmail.com>
2018-05-23 16:28:32 -0700

    pkease worj

--- firstCommit.js
+++ firstCommit.js
@@ -1,5 +1,8 @@
 console.log("hello world");
 
+
+console.log("Ima print something out up here now");
+
 //This is a recursive function that prints out a factorial of the number
 
 function factorial(someNumber) {
@@ -10,4 +13,3 @@ function factorial(someNumber) {
     }
 }
 
-console.log(factorial(3));
