d5ba72a3f0de19d7e8b6c4ec8293ff635b56579f
Phuc Dao <kanidd20@gmail.com>
2018-05-23 16:18:07 -0700

    This is the commit message

--- firstCommit.js
+++ firstCommit.js
@@ -1,1 +1,13 @@
 console.log("hello world");
+
+//This is a recursive function that prints out a factorial of the number
+
+function factorial(someNumber) {
+    if(someNumber == 1){
+        return someNumber
+    }else {
+        return someNumber * factorial(someNumber -1)
+    }
+}
+
+console.log(factorial(3));
